4ba7b0331cefa18704831b34a2feb50ac2f40232
DarshanHegde15 <96007698+DarshanHegde15@users.noreply.github.com>
2023-12-26 12:32:21 +0000

    Updated

---
+++ Basics/StringConcat.js
@@ -0,0 +1,7 @@
+let name="Darshan";
+let age=21;
+//Old style of string concatination
+console.log("My name is "+name+" and my age is "+age);
+
+//New and better method to improve readability
+console.log(`My name is ${name} and my age is ${age}`);
