e28f34c6d20adadbaf3303bea2eb2c0d5a3cc850
DarshanHegde15 <96007698+DarshanHegde15@users.noreply.github.com>
2023-12-27 01:41:01 +0000

    Updated

---
+++ Basics/DateTime.js
@@ -0,0 +1,14 @@
+const date=new Date();
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString());
+console.log(date.getDate());
+console.log(date.getDay());
+
+const dat=new Date('09-17-2001');
+console.log(dat.toLocaleString());
+
+const d=new Date(2001,8,17,13,23);
+console.log(d.toString());
